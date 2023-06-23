c691211b53a20bb780d8cf196368d345fdc6123f
nirapadak <naminopak@gmail.com>
2023-06-23 18:39:15 +0600

    fixed error

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+// loading
+document.addEventListener('DOMContentLoaded', function () {
+  var loadingOverlay = document.querySelector('.loading-overlay');
+  setTimeout(function() {
+    loadingOverlay.style.display = 'none';
+  }, 1000);
+});
+// themeToggle
+document.addEventListener('DOMContentLoaded', function() {
+  var themeToggle = document.getElementById('themeToggle');
+  var body = document.body;
 
-console.log("Js file started successfully")
+  themeToggle.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+  });
+});
