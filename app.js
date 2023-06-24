1df55d907b4c3ad3fdd24292185f49ad030c7723
nirapadak <naminopak@gmail.com>
2023-06-24 07:50:33 +0600

    img error fix

--- app.js
+++ app.js
@@ -14,3 +14,49 @@ document.addEventListener('DOMContentLoaded', function() {
     body.classList.toggle('dark-mode');
   });
 });
+
+
+// barcart
+
+document.addEventListener('DOMContentLoaded', function() {
+  var canvas = document.getElementById('barChart');
+  var ctx = canvas.getContext('2d');
+
+  // Data for the bar chart
+  var data = [
+    { label: 'Team A', value: 75 },
+    { label: 'Team B', value: 60 },
+    { label: 'Team C', value: 90 },
+    { label: 'Team D', value: 45 },
+    { label: 'Team E', value: 80 }
+  ];
+
+  // Chart properties
+  var chartWidth = 400;
+  var chartHeight = 300;
+  var barSpacing = 20;
+  var barColor = 'rgb(75, 132, 201)';
+
+  // Calculate bar width based on chart dimensions and data length
+  var barWidth = (chartWidth - barSpacing * (data.length - 1)) / data.length;
+
+  // Scale the chart to fit the data
+  var maxValue = Math.max(...data.map(item => item.value));
+  var scaleY = chartHeight / maxValue;
+
+  // Draw the bars
+  data.forEach(function(item, index) {
+    var barX = index * (barWidth + barSpacing);
+    var barHeight = item.value * scaleY;
+    var barY = chartHeight - barHeight;
+
+    ctx.fillStyle = barColor;
+    ctx.fillRect(barX, barY, barWidth, barHeight);
+
+    // Draw label below each bar
+    ctx.fillStyle = '#000';
+    ctx.font = '14px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText(item.label, barX + barWidth / 2, chartHeight + 20);
+  });
+});
